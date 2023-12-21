9e27d2d3de495f3d43b99bfbfc52520a6cfda8af
Vincent Klimjack <vklimjack@gmail.com>
2023-12-21 12:05:40 +0000

    add user validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 100 }
+
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followed, through: :followed_users
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save the user without email" do
+    user = User.new(password: '112231231211', name: 'bob')
+    assert_not user.save, 'saved the user with no email'
+  end
+
+  test "should not save the user without password" do
+    user = User.new(email: 'bob@email.com', name: 'bob')
+    assert_not user.save, 'saved the user with no password'
+  end
+
+  test "should not save the user with too short password" do
+    user = User.new(email: 'blah', name: 'bob', password: '1')
+    assert_not user.save, 'user saved with too short password'
+  end
+
+  test "should not save the user with no name" do
+    user = User.new(email:'bob', password: 'password123')
+    assert_not user.save, 'user saved with no name'
+  end
+
+  test "should not save the user with too long name" do
+    user = User.new(email:'bob', password: 'password123', name: SecureRandom.alphanumeric(101))
+    assert_not user.save, 'user saved with too long of a name'
+  end
+
+  test "user.posts should get associated posts" do
+    user = User.new(id: 1, name: 'bob', email: 'bob@test.com', password: 'password123')
+    posts = [Post.new(id: 1, body: 'body', user: user), Post.new(id: 2, body: 'body2', user: user)]
+
+    assert user.posts.all? { |post| posts.include?(post) }
+  end
 end
